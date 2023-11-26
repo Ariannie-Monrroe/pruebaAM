91f1823cb9b5f8a8332a21ae3d3a75fd7fe23600
Pepinder <101827444+Pepinder@users.noreply.github.com>
2023-11-25 21:36:50 -0300

    porfavor arranca app

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -79,7 +79,6 @@ export class RegisterPage implements OnInit {
 
   }
   async ngOnInit() {
-    // this.cameraService.initializeCamera();
     this.cargarRegion();
     this.registerForm?.get('idRegion')?.valueChanges.subscribe((idRegion) => {
       if (idRegion) {
@@ -121,55 +120,6 @@ export class RegisterPage implements OnInit {
     this.photoService.addNewToGallery().then((photo) => {
     });
   }
-
-  // async guardar() {
-  //   var f = this.registerForm.value;
-
-  //   if (!this.registerForm.valid) {
-  //     // console.log(this.registerForm.value);
-  //     this.alertController.create({
-  //       header: 'Ups',
-  //       message: 'Completa correctamente todos los campos',
-  //       buttons: ['Aceptar']
-  //     }).then(alert => alert.present());
-
-  //     Object.keys(this.registerForm.controls).forEach(key => {
-  //       const control = this.registerForm.get(key);
-
-  //       if (control && control.invalid) {
-  //         console.log(`Control '${key}' no es válido. Errores:`, control.errors);
-  //       }
-  //     });
-  //   } else {
-  //     this.alertController.create({
-  //       header: 'Registro exitoso',
-  //       message: '¡Felicitaciones! Ya estás registrado.',
-  //       buttons: ['OK']
-  //     }).then(alert => alert.present());
-  //     const locationSel = await this.getCurrentLocation();
-  //     const regionSeleccionada = this.regiones.find(region => region.id === this.idRegion);
-  //     const comunaSeleccionada = this.comunas.find(comuna => comuna.id === this.idComuna);
-
-  //     var usuario = {
-  //       nombre: f.nombre,
-  //       email: f.email,
-  //       password: f.password,
-  //       carrera: f.carrera,
-  //       rut: f.rut,
-  //       fechaNacimiento: f.fechaNacimiento,
-  //       photo: f.photo,
-  //       region: f.idRegion.nombre,
-  //       comuna: f.idComuna.nombre,
-  //       latitud: locationSel.latitude,
-  //       longitud: locationSel.longitude,
-  //     }
-  //     console.log(f.region);
-  //     this.userService.guardarUsuario(usuario);
-  //     console.log(usuario)
-  //     this.router.navigateByUrl("login");
-
-  //   }
-  // }
 
   async getCurrentLocation() {
     const location = await Geolocation.getCurrentPosition();
@@ -231,94 +181,4 @@ export class RegisterPage implements OnInit {
     this.navCtrl.back();
   }
 
-
-  // onSubmit() {
-  //     if (this.registerForm.valid) {
-  //       console.log('Formulario enviado con éxito:', this.registerForm.value);
-  //       this.alertController.create({
-  //         header: 'Registro exitoso',
-  //         message: '¡Felicitaciones! Ya estás registrado.',
-  //         buttons: ['OK']
-  //       }).then(alert => alert.present());
-  //     } else {
-  //       console.log('Formulario no válido');
-  //     }
-
-  //     this.userService.add(this.nombre);
-  //     this.userService.add(this.email);
-  //     this.userService.add(this.password);
-  //     this.userService.add(this.rut);
-  //     this.userService.add(this.fechaNacimiento);
-  //     this.formularioServ= " ";
-  // }
-
-
-
-
-  // (this.registerForm.invalid){
-  //   this.alertController.create({
-  //     header: 'Ups',
-  //     message: 'Completa correctamente todos los campos',
-  //     buttons: ['Aceptar']
-  //   }).then(alert => alert.present());
-
-  // return
-
-
-
-  // async scan() {
-  //   try {
-  //     const image = await Camera.getPhoto({
-  //       quality: 90,
-  //       allowEditing: true,
-  //       resultType: CameraResultType.Uri,
-  //       source: CameraSource.Camera,
-  //     });
-
-
-  //   } catch (error) {
-  //     console.log('Error en el scanner:', error);
-  //   }
-  // }
-
-  // async takePicture() {
-  //   try {
-  //     const image = await Camera.getPhoto({
-  //       quality: 90,
-  //       allowEditing: true,
-  //       resultType: CameraResultType.DataUrl,
-  //       source: CameraSource.Camera
-  //     });
-
-  //     console.log('Imagen capturada:', image);
-  //   } catch (error) {
-  //     console.error('Error al tomar la foto:', error);
-  //   }
-  // }
-
-  // validarRut(rut: string) {
-  //   if (!rut || rut.length < 3) {
-  //     return false;
-  //   }
-  //   const rutSinFormato = rut.replace(/[^0-9kK-]/g, '');
-  //   if (rutSinFormato.length < 3) {
-  //     return false;
-  //   }
-  //   const split = rutSinFormato.split('-');
-  //   if (split.length !== 2) {
-  //     return false;
-  //   }
-  //   const num = parseInt(split[0], 10);
-  //   const dgv = split[1];
-  //   const dvCalc = this.dv(num);
-  //   return dvCalc === dgv;
-  // }
-  // dv(T: any) {
-  //   let M = 0;
-  //   let S = 1;
-  //   for (; T; T = Math.floor(T / 10)) {
-  //     S = (S + (T % 10) * (9 - (M++ % 6))) % 11;
-  //   }
-  //   return S ? S - 1 : 'k';
-  // }
 }
